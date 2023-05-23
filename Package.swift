035b5e68323608b6e0490a540f1594524de7a70c
Charlie Wu <c.wu@webfm.net>
2023-05-23 14:35:37 +1000

    first commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "PDFTron",
+    products: [
+        .library(
+            name: "PDFTron",
+            targets: ["PDFNet", "Tools"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PDFNet",
+            url: "https://pdftron.s3.amazonaws.com/downloads/ios/packages/10.1.0-81991/PDFNet.xcframework.zip",
+            checksum: "4367525fc3a9bd9ffa37c17f32341a2f05aeda51042944731a46084efdca3053"),
+        .binaryTarget(
+            name: "Tools",
+            url: "https://pdftron.s3.amazonaws.com/downloads/ios/packages/10.1.0-81991/Tools.xcframework.zip",
+            checksum: "aedf97abbf26276dd09a638b88bdc80f7d19b0cf1ee4c45aabc590cec7614026"),
+    ]
+)
